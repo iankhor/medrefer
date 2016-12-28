ac1bc2306649324fc5bb8ec4ed68c4e04be8dfd2
iankhor <iankhorbc.thecoder@gmail.com>
2016-12-28 13:00:35 +1100

    establised mongo db connection

--- back-end/app.js
+++ back-end/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -7,7 +9,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var referral = require('./routes/referral_test')
+// var referral = require('./routes/referral_test')
+var referrals = require('./routes/referrals')
 
 var app = express();
 
@@ -25,7 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/referral_test', referral)
+// app.use('/referral_test', referral)
+app.use('/referrals', referrals)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- back-end/models/_init.js
+++ back-end/models/_init.js
@@ -5,6 +5,8 @@ const { connection: db } = mongoose
 
 db.on('error', console.error.bind(console, 'Connection error : '))
 
-db.once('open',console.long(console,'Connection established !'))
+db.once('open', () => {
+    console.log('Connection established')
+})
 
 module.exports = db
