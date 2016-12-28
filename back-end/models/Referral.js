dc152767cabb11dbb73ee348d7ec7cf9303e73fa
iankhor <iankhorbc.thecoder@gmail.com>
2016-12-28 12:38:49 +1100

    adding referral test code

--- back-end/models/Referral.js
+++ back-end/models/Referral.js
@@ -2,10 +2,15 @@ const mongooose = require('mongoose')
 const db = require('./_init')
 
 const referralSchema = new mongooose.Schema({
-    first_name: String,
-    last_name: String,
+    patient_first_name: String,
+    patient_last_name: String,
+    patient_referral_medical_condition: String,
+    doctor_first_name: String,
+    doctor_last_name: String,
+    doctor_address: String,
+    doctor_provider_number : Number,
+    doctor_signature: String,
     created_at: { type: Date, default: Date.now }
-
 })
 
 const Referral = mongooose.model('Referral', referralSchema)
--- back-end/routes/referral_test.js
+++ back-end/routes/referral_test.js
@@ -4,8 +4,14 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
-      first_name : 'John',
-      last_name: 'Doe'
+    patient_first_name: 'John',
+    patient_last_name: 'Doe',
+    patient_referral_medical_condition: 'Psycosis',
+    doctor_first_name: 'Greg',
+    doctor_last_name: 'Sam',
+    doctor_address: 'Badminton Connect',
+    doctor_provider_number : '123456',
+    doctor_signature: 'JPEG Link',
   })
 });
 
