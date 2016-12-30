374c979b111b022f9cabe4bed35b55ae4ae16396
iankhor <iankhorbc.thecoder@gmail.com>
2016-12-30 13:23:36 +1100

    updated referral and referal history schema

--- back-end/models/Referral.js
+++ back-end/models/Referral.js
@@ -1,18 +1,46 @@
-const mongooose = require('mongoose')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 const db = require('./_init')
 
-const referralSchema = new mongooose.Schema({
-    patient_first_name: String,
-    patient_last_name: String,
-    patient_referral_medical_condition: String,
-    doctor_first_name: String,
-    doctor_last_name: String,
-    doctor_address: String,
-    doctor_provider_number : Number,
-    doctor_signature: String,
-    created_at: { type: Date, default: Date.now }
+const referralSchema = new Schema({
+    isProxy:                        String,
+    isScheduled:                    String,
+    referredToStatus :              String,
+    ruralCode:                      String,
+    referredBy:                     [{  type:     Schema.Types.ObjectId, 
+                                        ref:    'ReferralHistory'     
+                                    }],
+    referredTo:                     [{  type:     Schema.Types.ObjectId, 
+                                        ref:    'ReferralHistory'     
+                                    }],
+    patientFirstName:               String,
+    patientLastName:                String,
+    patientAddress:                 String,
+    patientPhone:                   Number,
+    medicareNumber:                 Number,
+    reasonForReferral:              String,
+    reasonForReferralComments:      String,
+    presentingComplains:            String,
+    historyToPresentingComplains:   String,
+    treatmentsToDate:               String,
+    otherClinicalNotes:             String,
+    timeframeOfAssessment:          String,
+    category:                       String,
+    appointmentDate:                String,
+    appointmentTime:                String, 
+    created_at:                     {   type: Date,
+                                        default: Date.now 
+                                    }
 })
 
-const Referral = mongooose.model('Referral', referralSchema)
+const referralHistorySchema = new Schema({
+    created_at:                     {   type: Date,
+                                        default: Date.now 
+                                    }
+})
+
+const Referral = mongoose.model('Referral', referralSchema)
+const ReferralHistory = mongoose.model('ReferralHistory', referralHistorySchema)
 
 module.exports = Referral
+module.exports = ReferralHistory
