7282a4271b231ae1f2a24154657d937e29d22d42
iankhor <iankhorbc.thecoder@gmail.com>
2017-01-04 21:08:25 +1100

    updated index.js for injectTapEvents fix as it needed to be called once, added toolbar

--- front-end/src/components/ReferralApp/Referral/App.js
+++ front-end/src/components/ReferralApp/Referral/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react'
 import Header from '../../Shared/Header'
 import Form from './Form'
 import Footer from '../../Shared/Footer'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class Referral extends Component {
     render(){
         return(
+        <MuiThemeProvider>
             <div className="generic-center borderless"> 
                 Referral of Mr.[Insert a name] page
                 {/*<Header />*/}
                 <Form />
                 <Footer />
             </div>
+        </MuiThemeProvider>
         )
     }
 }
