2cc455fc222b70b74805e7c758fc30af454f10f6
iankhor <iankhorbc.thecoder@gmail.com>
2017-01-05 20:58:21 +1100

    added user authentication code

--- back-end/app.js
+++ back-end/app.js
@@ -1,18 +1,28 @@
-require('dotenv').config();
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV == null) {
+  require('dotenv').config(); // Load .env
+}
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//user authentication 
+const passport = require('passport');
+const session = require('express-session');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var referral = require('./routes/referral_test')
-var referrals = require('./routes/referrals')
+//models
+const User = require('./models/User');
 
-var app = express();
+//routes
+const index = require('./routes/index');
+const users = require('./routes/users');
+const referral = require('./routes/referral_test')
+const referrals = require('./routes/referrals')
+const auth = require('./routes/auth');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,10 +36,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//for user authentication
+app.use(session({ secret: process.env.SESSION_SECRET }));
+
+// Passport + User
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// Express + Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/referral_test', referral)
 app.use('/referrals', referrals)
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
