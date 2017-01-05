047130c527d6c8f8edd593f683622e0c1145db03
iankhor <iankhorbc.thecoder@gmail.com>
2017-01-05 21:45:13 +1100

    updated templates, added modal button

--- front-end/src/components/Login/App.js
+++ front-end/src/components/Login/App.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react'
-import Header from './Header'
-import Body from './Body'
-import Footer from './Footer'
+import LoginButton from './LoginButton'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-class App extends Component {
+
+class Login extends Component {
     render(){
         return(
             <MuiThemeProvider>
                 <div className="generic-center borderless"> 
-                    App page
-                    <Header />
-                    <Body />
-                    <Footer />
+                    Login page
+                    <LoginButton />
                 </div>
             </MuiThemeProvider>
         )
     }
 }
 
-export default App
+export default Login
 
